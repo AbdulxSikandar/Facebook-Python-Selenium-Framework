4d2ad474624342c11f9c73366ac2049d9965660e
Sikandar <abdulxsikandar@gmail.com>
2022-07-05 15:55:05 +0530

    Create Screenshot.py

---
+++ Screenshots/Screenshot.py
@@ -0,0 +1,13 @@
+
+# This is used to take automatically screenshot
+import pytest
+
+
+# @pytest.fixture("scope = class")
+# class Screenshot():
+#
+#     def __init__(self, driver):
+#       self.driver = driver
+#
+#     def _capture_screenshot(self,name):
+#          self.driver.get_screenshot_as_file(name)
